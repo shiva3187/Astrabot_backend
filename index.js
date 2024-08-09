176d5a3c2a132ddf6adae7d9803a1f61878f6e58
shiva3187 <shivansh3187@gmail.com>
2024-08-09 17:34:40 +0530

    First Commit

--- index.js
+++ index.js
@@ -51,7 +51,9 @@ const imagekit = new ImageKit({
   publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
   privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
 });
-
+app.get("/",(req,res)=>{
+  res.send('Hello');
+});
 app.get("/api/upload", (req, res) => {
   const result = imagekit.getAuthenticationParameters();
   res.send(result);
