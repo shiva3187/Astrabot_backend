43769a3b67bfed51466ba10899e9736023686c0f
shiva3187 <shivansh3187@gmail.com>
2024-08-09 17:53:09 +0530

    index.js

--- index.js
+++ index.js
@@ -174,9 +174,9 @@ app.use((err, req, res, next) => {
 // PRODUCTION
 app.use(express.static(path.join(__dirname, "../client")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client", "index.html"));
+// });
 
 app.listen(port, () => {
   connect();
