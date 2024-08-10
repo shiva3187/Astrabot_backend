715c5b8b71dc67e2e72893ce34cbd4bf38f902da
shiva3187 <shivansh3187@gmail.com>
2024-08-10 11:48:36 +0530

    gg

--- index.js
+++ index.js
@@ -179,6 +179,6 @@ app.use((err, req, res, next) => {
 // Serve static files for production
 app.use(express.static(path.join(__dirname, "../client")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client", "index.html"));
+});
